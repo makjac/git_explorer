fdd9585a2e436564bda99b67f4a75b9f2779f93b
makjac <maks.jackowski@proton.me>
2025-03-12 21:57:58 +0100

    chore: update temp page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:git_explorer/core/theme/app_theme.dart';
 import 'package:git_explorer/core/theme/theme_cubit.dart';
 import 'package:git_explorer/core/theme/theme_extension.dart';
+import 'package:git_explorer/core/widgets/icons/app_icon.dart';
+import 'package:git_explorer/core/widgets/icons/app_icons.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -30,15 +32,29 @@ class Temp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('temp')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.read<ThemeCubit>().changeTheme(
-              context.theme.brightness == Brightness.dark
-                  ? ThemeMode.light
-                  : ThemeMode.dark,
-            );
-          },
-          child: const Text('Change Theme'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.read<ThemeCubit>().changeTheme(
+                  context.theme.brightness == Brightness.dark
+                      ? ThemeMode.light
+                      : ThemeMode.dark,
+                );
+              },
+              child: const Text('Change Theme'),
+            ),
+            const AppIcon(icon: AppIcons.branch),
+            const AppIcon(icon: AppIcons.code),
+            const AppIcon(icon: AppIcons.frok),
+            const AppIcon(icon: AppIcons.pullRequest),
+            const AppIcon(icon: AppIcons.readme),
+            const AppIcon(icon: AppIcons.search),
+            const AppIcon(icon: AppIcons.star),
+            const AppIcon(icon: AppIcons.settings),
+            const AppIcon(icon: AppIcons.tag),
+            const AppIcon(icon: AppIcons.watch),
+          ],
         ),
       ),
     );
