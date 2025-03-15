fcec20cdd8f0756c0f2181db21a1477d44c6714f
makjac <maks.jackowski@proton.me>
2025-03-15 14:58:13 +0100

    feat: create repo_details_widgets file to export repository detail components

---
+++ lib/pages/repo_details/widgets/repo_details_widgets.dart
@@ -0,0 +1,6 @@
+export 'description/repo_description.dart';
+export 'header/repo_header.dart';
+export 'stats/repo_stats.dart';
+export 'status_indicator/status_indicator.dart';
+export 'topics/repo_topics.dart';
+export 'tree/repo_tree_list.dart';
