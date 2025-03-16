efd4c00626493a044e738dd66a6f75a2d9297e5d
makjac <maks.jackowski@proton.me>
2025-03-16 01:34:05 +0100

    feat: add RepoPullRequestWidget for exporting pull request components

---
+++ lib/pages/repo_pull_requests/widgets/repo_pull_request_widget.dart
@@ -0,0 +1,4 @@
+export 'header/repo_pull_requests_header.dart';
+export 'list/repo_pull_requests_list.dart';
+export 'status_indicator/status_indicator.dart';
+export 'thumbnail/repo_pull_request_thumbnail.dart';
